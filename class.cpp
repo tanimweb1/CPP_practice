1f7df6e306a94994da3dec500f0c209d564d488c
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-14 02:00:32 +0600

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,8 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+namespace A{
+    void display(){
+        cout<<"hello";
+    }
+}
+namespace B{
+    void display(){
+        cout<<"hi";
+    }
+}
+
+
 int main(){
 
-
+A::display();
+B::display();
 
 
     return 0;
