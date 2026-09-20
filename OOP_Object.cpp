1f582bf66cc2cb7fd2efabe1282f28b30208c2b8
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-09-20 14:55:35 +0600

    Add files via upload

---
+++ OOP_Object.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+public:
+string name;
+
+
+};
+
+int main(){
+Student S;
+S.name = "Tanim";
+cout<<S.name<<endl;
+
+
+
+
+
+
+    return 0;
+}
